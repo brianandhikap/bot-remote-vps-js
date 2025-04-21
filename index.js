55ae8e205eee824b61df20660c38f2d390b48bef
Brian Andhika Putra <135279979+brianandhikap@users.noreply.github.com>
2025-04-21 19:46:25 +0700

    add debug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const handleCommand = require('./commands');
-const { allowedUsers, allowedGroup } = require('./config');
+const { allowedUser, allowedGroup } = require('./config');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -21,30 +21,25 @@ client.on('ready', () => {
 
 client.on('message', async (msg) => {
   const { from, body, author } = msg;
-  const sender = msg.fromMe ? from : (author || from);
+  const sender = msg.fromMe ? allowedUser : (author || from);
 
-  const isPrivateChat = from.endsWith('@c.us');
-  const isGroupChat = from.endsWith('@g.us');
-
+  //debug
   console.log('📩 Incoming message:');
   console.log(`From       : ${from}`);
   console.log(`Author     : ${author}`);
   console.log(`Sender used: ${sender}`);
   console.log(`Body       : ${body}`);
-  console.log(`Private?   : ${isPrivateChat}`);
-  console.log(`Group?     : ${isGroupChat}`);
+  console.log(`isGroup?   : ${msg.isGroupMsg}`);
   console.log('====================');
 
-  const isPrivateAllowed = isPrivateChat && allowedUsers.includes(from);
-  const isGroupAllowed = isGroupChat && from === allowedGroup && allowedUsers.includes(sender);
+  const isPrivateAllowed = from === allowedUser;
+  const isGroupAllowed = from === allowedGroup && sender === allowedUser;
 
-  if (body.startsWith('!')) {
-    if (isPrivateAllowed || isGroupAllowed) {
-      handleCommand(msg, body.trim(), (reply) => msg.reply(reply));
-    } else {
-      msg.reply('Kamu siapa?');
-    }
+  if (isPrivateAllowed || isGroupAllowed) {
+    handleCommand(msg, body.trim(), (reply) => msg.reply(reply));
+
+  } else {
+    msg.reply('Kamu siapa?');
   }
 });
-
 client.initialize();
