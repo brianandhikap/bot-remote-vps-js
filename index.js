4b32256df4c023ef6bf23e2e509d8a8eda28bfe8
Brian Andhika Putra <135279979+brianandhikap@users.noreply.github.com>
2025-04-21 14:56:05 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const handleCommand = require('./commands');
+const { allowedUser, allowedGroup } = require('./config');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true
+    }
+});
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('✅ Bot is ready!');
+});
+
+client.on('message', async (msg) => {
+  const { from, body, author } = msg;
+  const sender = msg.fromMe ? allowedUser : (author || from); // author jika group, from jika personal
+
+  const isPrivateAllowed = from === allowedUser;
+  const isGroupAllowed = from === allowedGroup && sender === allowedUser;
+
+  if (isPrivateAllowed || isGroupAllowed) {
+    handleCommand(msg, body.trim(), (reply) => msg.reply(reply));
+
+  } else {
+    msg.reply('Kamu siapa?');
+  }
+});
+
+client.initialize();
